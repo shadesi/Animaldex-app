5ab06a378b6a807ee781b57684a825f5d5be0c23
shadesi <45621420+shadesi@users.noreply.github.com>
2024-07-02 00:21:45 +0200

    Update scripts.js
    Added for loop that iterates through the AnimalList array
    Output -> height for each animal

--- scripts.js
+++ scripts.js
@@ -17,3 +17,27 @@ let animalList = [
     }
   
 ];
+
+
+// Iterate over each Animal in the animalList array
+for (let i = 0; i < animalList.length; i++) {
+    let animal = animalList[i];
+
+    // Get the current Animal's name and height
+    if(animalList[i].height < 7 && animalList[i].height > 5) {
+
+        // Write the output to the website's DOM
+        document.write(animal.name+ " [ height: " +animal.height + " ]"+" is classified as a medium size animal!" + '<br><br>');
+    }else if (animalList[i].height < 6 && animalList[i].height > 4){
+
+        // Write the output to the website's DOM
+        document.write(animal.name+ " [ height: " +animal.height + " ]"+" is classified as a small size animal!" + '<br>');
+
+    } else {
+
+        // Write the output to the website's DOM
+        document.write(animal.name+ " [ height: " +animal.height + " ]"+ " is classified as a big size animal!" + '<br><br>'); 
+    }
+
+
+}
