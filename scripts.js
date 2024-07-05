5c5f45f3a7f7cd9c5d5527ebd7ce4b31543d92ca
shadesi <45621420+shadesi@users.noreply.github.com>
2024-07-05 02:26:01 +0200

    Update scripts.js
    Implemented IIFE and Repository pattern
    Replaced for loop with forEach
    Added new animals 
    Adjusted the code to show their height accordingly

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
-// This array will contain Animals 
-let animalList = [
+// Part 2: Implementing IIFE and Repository Pattern
+let animalRepository = (function () {
+  let animalList = [
     {
         name: "Lion",
         height: 7,
@@ -15,29 +16,37 @@ let animalList = [
         height: 5,
         types: ['water']
     }
-  
-];
+  ];
 
+  function add(animal) {
+    animalList.push(animal);
+  }
 
-// Iterate over each Animal in the animalList array
-for (let i = 0; i < animalList.length; i++) {
-    let animal = animalList[i];
+  function getAll() {
+    return animalList;
+  }
 
-    // Get the current Animal's name and height
-    if(animalList[i].height < 7 && animalList[i].height > 5) {
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
 
-        // Write the output to the website's DOM
-        document.write(animal.name+ " [ height: " +animal.height + " ]"+" is classified as a medium size animal!" + '<br><br>');
-    }else if (animalList[i].height < 6 && animalList[i].height > 4){
+// Part 1: Refactor to Use forEach
+// Adding some sample Animals to the repository
+animalRepository.add({ name: 'Cat', height: 2, types: ['tree', 'leaves'] });
+animalRepository.add({ name: 'Rabbit', height: 3, types: ['wool'] });
+animalRepository.add({ name: 'Mouse', height: 1, types: ['Cheese'] });
 
-        // Write the output to the website's DOM
-        document.write(animal.name+ " [ height: " +animal.height + " ]"+" is classified as a small size animal!" + '<br>');
-
-    } else {
-
-        // Write the output to the website's DOM
-        document.write(animal.name+ " [ height: " +animal.height + " ]"+ " is classified as a big size animal!" + '<br><br>'); 
-    }
-
-
-}
+// Using forEach to iterate over animalList and print each Animal's details
+animalRepository.getAll().forEach(function(animal) {
+  if (animal.height >= 6) {
+    document.write(animal.name + " [ height: " + animal.height + " ]" + " is classified as a big size animal!" + '<br><br>');
+  } else if (animal.height >= 4 && animal.height < 6) {
+    document.write(animal.name + " [ height: " + animal.height + " ]" + " is classified as a medium size animal!" + '<br><br>');
+  } else if (animal.height >= 2 && animal.height < 4) {
+    document.write(animal.name + " [ height: " + animal.height + " ]" + " is classified as a small size animal!" + '<br><br>');
+  } else {
+    document.write(animal.name + " [ height: " + animal.height + " ]" + " is classified as a tiny size animal!" + '<br><br>');
+  }
+});
